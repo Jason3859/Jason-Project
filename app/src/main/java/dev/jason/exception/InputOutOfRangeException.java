1bb6bfd8238fbbdded0a2234f4cfc9e3970820ff
Jason3859 <augustinej548@gmail.com>
2025-04-15 14:46:56 +0530

    Remove all

--- app/src/main/java/dev/jason/exception/InputOutOfRangeException.java
+++
@@ -1,7 +0,0 @@
-package dev.jason.exception;
-
-public class InputOutOfRangeException extends Exception {
-    public InputOutOfRangeException(String message) {
-        super(message);
-    }
-}
--- settings.gradle.kts
+++
@@ -1,2 +0,0 @@
-rootProject.name = "Jason-Project"
-include("app")
